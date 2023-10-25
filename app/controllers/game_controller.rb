ee9010424c42b23e0656c0b2a43caa38f0431363
btlim17 <146046964+btlim17@users.noreply.github.com>
2023-10-25 16:22:19 +0000

    rails-dynamic-dice-roll

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,16 @@
+class GameController < ApplicationController
+  
+  def home
+    render({:template => "game_template/home"})
+  end
+
+  def flex
+    @dice = params.fetch("num_dice").to_i
+    @sides = params.fetch("num_sides").to_i
+    @roll = []
+    @dice.times do
+      @die = rand(1..@sides)
+      @roll.push(@die)
+    end
+    render({:template => "game_template/flex"})
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-
+get("/", {:controller => "game", :action => "home"})
+get("/:num_dice/:num_sides", {:controller => "game", :action => "flex"})
 end
